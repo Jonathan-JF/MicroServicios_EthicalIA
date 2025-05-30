883f16b86989c6e332d2568e314ae39b1cf060cc
Jonathan <jonathan30012@hotmail.com>
2025-05-30 19:45:14 -0400

    Add files via upload

---
+++ originalidad/src/main/java/MC_EthicalIA/originalidad/OriginalidadApplication.java
@@ -0,0 +1,13 @@
+package MC_EthicalIA.originalidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginalidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriginalidadApplication.class, args);
+	}
+
+}
---
+++ originalidad/src/test/java/MC_EthicalIA/originalidad/OriginalidadApplicationTests.java
@@ -0,0 +1,13 @@
+package MC_EthicalIA.originalidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginalidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
