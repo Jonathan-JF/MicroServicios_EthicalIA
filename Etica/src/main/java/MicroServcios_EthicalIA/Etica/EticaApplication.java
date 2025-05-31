1060a9e2e50b2539c57e95bd628eadc9f26bbccb
Jonathan <jonathan30012@hotmail.com>
2025-05-31 12:29:23 -0400

    Add files via upload

---
+++ Etica/src/main/java/MicroServcios_EthicalIA/Etica/EticaApplication.java
@@ -0,0 +1,13 @@
+package MicroServcios_EthicalIA.Etica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EticaApplication.class, args);
+	}
+
+}
---
+++ Etica/src/test/java/MicroServcios_EthicalIA/Etica/EticaApplicationTests.java
@@ -0,0 +1,13 @@
+package MicroServcios_EthicalIA.Etica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
