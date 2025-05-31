38e8f866c053c83f789d0d0b129fbb3aba4d1ad2
Jonathan <jonathan30012@hotmail.com>
2025-05-31 12:31:04 -0400

    se agrega el microservicio Coherencia

---
+++ Coherencia/src/test/java/MicroServcios_EthicalIA/Coherencia/CoherenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package MicroServcios_EthicalIA.Coherencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoherenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
